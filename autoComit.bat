bfa899113b02eee886c0a2f057e2cbb66fa0094b
Anatolie Jentimir <157539682+jentimanatol@users.noreply.github.com>
2025-07-13 17:32:33 -0400

    Missing or incompatible Matplotlib backend in .exe bundle:
    Missing or incompatible Matplotlib backend in .exe bundle:

--- autoComit.bat
+++ autoComit.bat
@@ -4,6 +4,6 @@ git commit -m " updted interface used CSS stiles "
 git push origin main
 
 :: === Tagging for GitHub Actions Release Build ===
-git tag v2.0
-git push origin v2.0
+git tag v2.1
+git push origin v2.1
 pause
