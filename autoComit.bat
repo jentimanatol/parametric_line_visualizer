7fb6e3b18853595750462268d3df2251d06579fd
Anatolie Jentimir <157539682+jentimanatol@users.noreply.github.com>
2025-07-13 17:41:28 -0400

    updted interface used CSS stiles

--- autoComit.bat
+++ autoComit.bat
@@ -4,6 +4,6 @@ git commit -m " updted interface used CSS stiles "
 git push origin main
 
 :: === Tagging for GitHub Actions Release Build ===
-git tag v2.3
-git push origin v2.3
+git tag v2.4
+git push origin v2.4
 pause
