d5a96e1b8c769f47e9bb923bfd522e977ab6a463
Anatolie Jentimir <157539682+jentimanatol@users.noreply.github.com>
2025-07-13 18:17:42 -0400

    Ensure plot window works in the frozen .exe
    Ensure plot window works in the frozen .exe

--- autoComit.bat
+++ autoComit.bat
@@ -1,9 +1,9 @@
 git --version
 git add .
-git commit -m " updted interface used CSS stiles "
+git commit -m " Ensure plot window works in the frozen .exe"
 git push origin main
 
 :: === Tagging for GitHub Actions Release Build ===
-git tag v2.7
-git push origin v2.7
+git tag v2.8
+git push origin v2.8
 pause
